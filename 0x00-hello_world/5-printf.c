2492d8e0b9e253f77e1f92c110a729889c8126e2
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-10 11:09:07 -0700

    5. Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * Write a C program that prints exactly:
  *
- * with proper grammar, but the outcome is a piece of art, 
+ * with proper grammar, but the outcome is a piece of art,
  *
  * followed by a new line.
  */
 int main(void)
 {
-     printf("with proper grammar, but the outcome is a piece of art,\n");
-     return(0);
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return(0);
 }
