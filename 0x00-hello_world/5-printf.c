8d38ab9c89df942c91b98916e760e338146b61fb
I-yan-u <iyanuajimobi12@gmail.com>
2022-09-10 04:16:32 -0700

    5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,8 @@
  * with proper grammar, but the outcome is a piece of art, 
  * followed by a new line.
  */
-
 int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art,");
- 
   return(0);
 }
